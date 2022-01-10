3acbac4430874642b0d8fcde92db36fbf2d8ce75
nguyengiatam <nguyengiatam.95@gmail.com>
2022-01-11 05:20:19 +0700

    update index

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-//require('dotenv').config();
+require('dotenv').config();
 const http = require("http");
 const url = require('url');
-const fs = require("fs");
+const {readFile} = require("fs/promises");
 const mime = require('mime-types');
 const {getIndex} = require('./getFile');
 const PORT = process.env.PORT || 3000;
 
-const sever = http.createServer((req, res) => {
+const sever = http.createServer(async (req, res) => {
   const path = url.parse(req.url, true);
   let pathName = path.pathname;
-
   if (pathName === "/") {
     res.writeHead(200, {'Content-type' : 'text/html'});
-    res.write(getIndex());
+    res.write(await getIndex());
   } else {
     res.writeHead(200, getMime(pathName));
-    res.write(getData(`./${pathName}`));
+    res.write(await getData(`./${pathName}`));
   }
   res.end();
 });
@@ -27,14 +26,12 @@ const getMime = path => {
   return mimeType;
 }
 
-const getData = fileName => {
-  let data = '';
+const getData = async fileName => {
   try {
-    data = fs.readFileSync(fileName);
+    return readFile(fileName);
   } catch (error) {
     console.log(error);
   }
-  return data;
 };
 
 sever.listen(PORT, () => {
